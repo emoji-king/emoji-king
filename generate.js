a83f2a6198ece287ff90388278474d3219084cdc
Emoji King 👑 <emojikingjs@gmail.com>
2017-02-01 14:09:58 -0800

    Update code to support fullyQualified.

--- generate.js
+++ generate.js
@@ -3,26 +3,54 @@ import agent from 'superagent'
 
 async function generate(url) {
   if (!url) url = 'http://www.unicode.org/Public/emoji/5.0/emoji-test.txt'
-
   const list = (await agent.get(url)).text
 
-  let emojis = {}
+  let names = {}
   for (const l of list.split('\n')) {
-    const m = l.match(/^[0-9a-fA-F]+.*;.*#\s+(\S+)/)
+    const m = l.match(/^[0-9a-fA-F]+.*;\s+(\S+)\s*#\s*(\S+)\s+(.+)/)
     if (m) {
-      const emoji = m[1]
-      let map = emojis
+      let status = m[1]
+      let emoji = m[2]
+      let name = m[3]
+
+      // There are errors in the list, here we fix them.
+      if ((emoji == '🏘️') && (name == 'house')) {
+        name = 'house buildings'
+      }
+
+      if (!names[name]) names[name] = {}
+      if (status == 'fully-qualified') {
+        if (!names[name].fq) {
+          names[name].fq = emoji
+        } else {
+          throw new Error(`Duplicate fully-qualified emoji for ${name} ${emoji}.`)
+        }
+      }
+      if (status != 'fully-qualified' && status != 'non-fully-qualified') {
+        throw new Error(`Invalid status ${status} for ${name} ${emoji}.`)
+      }
+      names[name].em = (names[name].em || []).concat(emoji)
+    }
+  }
+
+  let prefixes = {}
+  for (const name in names) {
+    for (const emoji of names[name].em) {
+
+      let prefix = prefixes
       for (let i = 0; i < emoji.length; ++i) {
         const c = emoji.charCodeAt(i)
-        if (map[c] == undefined) map[c] = {}
-        map = map[c]
+        if (prefix[c] == undefined) prefix[c] = {}
+        prefix = prefix[c]
       }
-      map['em'] = emoji
+      prefix['em'] = emoji
+      prefix['fq'] = names[name].fq
+      prefix['nm'] = name
     }
   }
 
   console.log('module.exports = ')
-  console.log(JSON.stringify(emojis))
+  console.log(JSON.stringify(prefixes))
 }
 
 generate()
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-const emojimap = require('./emoji-map')
+const emojimap = require('../lib/emoji-map')
 
-function parse(s, counts) {
+function getFullyQualified(emoji) {
+  let m = emojimap
+  let fq
+  while (emoji && (m = m[emoji.charCodeAt(0)])) {
+    if (m.fq) {
+      fq = m.fq
+    }
+
+    emoji = emoji.slice(1)
+  }
+
+  return fq
+}
+
+function count(s, opts) {
+
+  if (!opts) opts = {}
 
   function recurse(m, s) {
-    let x = s
     if (s && (m = m[s.charCodeAt(0)])) {
       s = s.slice(1)
       const emoji = recurse(m, s)
@@ -13,12 +28,16 @@ function parse(s, counts) {
     return undefined
   }
 
-  if (counts == undefined) counts = {}
+  let counts = opts.counts || {}
 
   while(s && s.length > 0) {
     const emoji = recurse(emojimap, s)
     if (emoji) {
-      counts[emoji] = (counts[emoji] || 0) + 1
+      let e = emoji
+      if (opts.fullyQualified) {
+        e = getFullyQualified(emoji)
+      }
+      counts[e] = (counts[e] || 0) + 1
     }
     s = s.slice((emoji || "1").length)
   }
@@ -27,5 +46,5 @@ function parse(s, counts) {
 }
 
 module.exports = {
-  parse
+  count
 }
